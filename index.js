780c6b4f1692ec68a584177919a77500d5d7a1bb
Ryan Hardin <ryanwhardin@gmail.com>
2020-02-19 21:32:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
 }
 
 function deleteFromObjectByKey(object,key){
-  var newObj = recipes
+  var newObj = Object.assign({},object,{[key]})
   delete object.prop
   return newObj
 }
